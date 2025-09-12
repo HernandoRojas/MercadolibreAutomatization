3de0edf67132d221ea7e5d8d8ea15fdbc643ab2e
Hernando José Rojas Castro <hjrcastro@outlook.es>
2025-09-12 15:42:36 -0500

    Se eliminan dependencias de JUnit, se usa únicamente TestNG con Cucumber y Allure para reportes

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,18 +1,21 @@
 package runner;
-import org.junit.AfterClass;
-import org.junit.runner.RunWith;
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import org.junit.AfterClass;
+//import org.junit.runner.RunWith;
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
 import pages.BasePage;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/features", //Directorio de archivos .features
     glue = "steps", //Directorio donde están las clases dfiniendo los steps escritos en el feature file
     plugin = {"pretty","html:target/cucumber-reports", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}, tags = "@SaberCategorias")
 
-//Comando para correr el tag deseado: gradle test -D  "cucumber.filter.tags=@Tag"
+//Comando para correr el tag deseado: ./gradlew test -D  "cucumber.filter.tags=@Tag"
 
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests{
     @AfterClass
     public static void limpiarDriver(){
         BasePage.cerrarNavegador();
--- src/test/java/steps/BusquedaSteps.java
+++ src/test/java/steps/BusquedaSteps.java
@@ -3,22 +3,28 @@ package steps;
 import org.testng.Assert;
 
 import io.cucumber.java.en.*;
+import io.qameta.allure.*;
 import pages.ConfigReader;
 import pages.PaginaArticulo;
 import pages.PaginaPrincipal;
 import pages.PaginaResultadosBusqueda;
 
+@Epic("Mercado Libre Test")
+@Feature("Búsqueda de Productos")
+@Story("El usuario puede buscar por nombre o palabra clave")
 public class BusquedaSteps {
 
     PaginaPrincipal paginaPrincipal = new PaginaPrincipal();
     PaginaResultadosBusqueda paginaResultados = new PaginaResultadosBusqueda();
     PaginaArticulo paginaArticulo = new PaginaArticulo();
     ConfigReader configReader = new ConfigReader();
-    
+
+    @Step("Dado que el usuario navega a la pagina principal de mercado libre")
     @Given("El usuario navega a la pagina www.mercadolibre.com.co")
     public void irAUrlPaginaPrincipal(){  
     //Ir a pagina principal de mercado libre
         paginaPrincipal.irAPaginaPrincipal();
+        Allure.step("Navegando a la URL inicial de mercado libre");
     }
 
     @When("busca la palabra {word}")
@@ -38,22 +44,26 @@ public class BusquedaSteps {
 
     // Steps para escenario número 2 @BuscarArticulos
 
+    @Step("Cuando ingrese el artículo de su interés en la barra de búsqueda")
     @When("busque {string} en el campo de busqueda")
     public void buscarArticulos(String textoParaBusqueda){
         //Busca el artículo por palabra clave ingresando la palabra en el campo de texto de busqueda
         paginaPrincipal.escribirEnBarraDeBusqueda(textoParaBusqueda);
         paginaPrincipal.clicBotonBarraDeBusqueda();
+        Allure.step("El usuario ingresa la palabra " + textoParaBusqueda + " y da clic en buscar");
     }
 
+    @Step("El sistema mostrará resultados para el artículo buscado")
     @Then("el sistema muestra resultados para cada {string}")
     public void validarBusquedaArticulos(String titulo){
         //Se valida que el articulo seleccionado tenga por título el esperado
         String tituloExistente = paginaResultados.obtenerTituloResultado();
         String tituloEsperado = titulo;
         Assert.assertEquals(tituloExistente,tituloEsperado);
+        Allure.addAttachment("Se verifica el artículo buscado", "Se han encontrado resultados para: " + tituloEsperado);
     }
 
-    // Steps para escenario número 2 @BuscarArticuloEspecifico
+    // Steps para escenario número 3 @BuscarArticuloEspecifico
 
     @When("busque el articulo {word}")
     public void buscarArticuloMancuerna(String palabra){
